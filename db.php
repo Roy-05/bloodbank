c70f9fd660a40b5579f43e3a41bc1b356cafd007
Saket Roy <roy05@live.missouristate.edu>
2020-11-29 18:50:41 +0530

    Set up initial sign up page

--- db.php
+++ db.php
@@ -3,14 +3,14 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_PASSWORD', '$t3isTemp');
+define('DB_NAME', 'bloodbank');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
-if($link === false){
+if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
