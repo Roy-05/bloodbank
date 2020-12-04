5aeb1057c1aa34bad7491e6e68791f4c09d15f50
Saket Roy <roy05@live.missouristate.edu>
2020-12-04 17:26:15 +0530

    relative paths

--- components/head.php
+++ components/head.php
@@ -6,13 +6,13 @@
 <!-- Header tags that need to be included in every page: -->
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<link rel="shortcut icon" href="/bloodbank/assets/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" />
-<link rel="stylesheet" href="/bloodbank/css/main.css">
+<link rel="stylesheet" href="./css/main.css">
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" defer></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" defer></script>
-<script src= "/bloodbank/main.js"></script>
+<script src= "./main.js"></script>
 <!-- End Scripts-->
